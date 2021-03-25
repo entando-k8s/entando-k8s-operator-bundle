b08856be4e8d3bf81174ec19c595bc293e5827c9
Ampie Barnard <ampie.private@gmail.com>
2021-03-25 11:10:38 +0200

    Fixed version logic

--- set-csv-version.sh
+++ set-csv-version.sh
@@ -6,7 +6,7 @@ if [[ -z "$new_version" ]]; then
   new_version=$(cat ./VERSION)
 fi
 old_version=$(cat manifests/entando-k8s-operator.v0.3.x.clusterserviceversion.yaml | grep "name: entando-k8s-operator.v" | sed 's/.*v\(.*\)/\1/')
-sed -i "s/$old_version/$VERSION/g" manifests/entando-k8s-operator.v0.3.x.clusterserviceversion.yaml
+sed -i "s/$old_version/$new_version/g" manifests/entando-k8s-operator.v0.3.x.clusterserviceversion.yaml
 #old_version=${operator_name}
 #export major_minor=${old_version%.*}
 #echo $major_minor
