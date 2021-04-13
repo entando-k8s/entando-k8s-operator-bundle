f399ce7ee9166f293f90da8fe83f5ff936952a72
Ampie Barnard <ampie.private@gmail.com>
2021-04-13 14:07:23 +0200

    Tweaked scripts to facilitate the process

--- publish-for-certification.sh
+++ publish-for-certification.sh
@@ -1,4 +1,4 @@
-export MY_VERSION=0.3.34
-docker build . -t entando/entando-k8s-operator-bundle:${MY_VERSION} \
-      && docker push entando/entando-k8s-operator-bundle:${MY_VERSION} \
-      && operator-sdk bundle validate docker.io/entando/entando-k8s-operator-bundle:${MY_VERSION}
+export RELEASE_VERSION=6.3.1
+docker build . -f Dockerfile.rh-cert -t entando/entando-k8s-operator-bundle:${RELEASE_VERSION} \
+      && docker push entando/entando-k8s-operator-bundle:${RELEASE_VERSION} \
+      && operator-sdk bundle validate docker.io/entando/entando-k8s-operator-bundle:${RELEASE_VERSION}
