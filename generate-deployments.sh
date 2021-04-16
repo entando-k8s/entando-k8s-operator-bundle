6a28eb180606202303bcd11416c8583f87578442
Ampie Barnard <ampie.private@gmail.com>
2021-04-16 08:51:16 +0200

    Modified olm template for RH Cert

--- generate-deployments.sh
+++ generate-deployments.sh
@@ -20,14 +20,19 @@ function writeAllResourcesForVersion {
   local k8s_version=$1
   local output_dir=./manifests/${k8s_version}/namespace-scoped-deployment
   rm $output_dir/*.yaml 2>/dev/null
+  if [ $k8s_version == "k8s-before-116" ]; then
+    support_openshift_311="true"
+  else
+    support_openshift_311="false"
+  fi
   writeClusterResourceToFile ${k8s_version} ${output_dir}/cluster-resources.yaml
-  helm template --set operator.supportOpenshift=false,operator.clusterScope=false,bundle.olmDisabled=true ./  >> ${output_dir}/namespace-resources.yaml
+  helm template --set operator.supportOpenshift311=${support_openshift_311},operator.clusterScope=false,bundle.olmDisabled=true ./  >> ${output_dir}/namespace-resources.yaml
   writeClusterResourceToFile ${k8s_version} ${output_dir}/all-in-one.yaml
-  helm template --set operator.supportOpenshift=false,operator.clusterScope=false,bundle.olmDisabled=true ./  >> ${output_dir}/all-in-one.yaml
+  helm template --set operator.supportOpenshift311=${support_openshift_311},operator.clusterScope=false,bundle.olmDisabled=true ./  >> ${output_dir}/all-in-one.yaml
   output_dir=./manifests/${k8s_version}/cluster-scoped-deployment
   rm $output_dir/*.yaml 2>/dev/null
   writeClusterResourceToFile ${k8s_version} ${output_dir}/all-in-one.yaml
-  helm template --set operator.supportOpenshift=false,operator.clusterScope=true,bundle.olmDisabled=true ./  >> ${output_dir}/all-in-one.yaml
+  helm template --set operator.supportOpenshift311=${support_openshift_311},operator.clusterScope=true,bundle.olmDisabled=true ./  >> ${output_dir}/all-in-one.yaml
 }
 
 writeAllResourcesForVersion k8s-116-and-later
