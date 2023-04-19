caded44a6abae72baf1b2c3399f06e19fd34ed63
Luigi Sportelli <l.sportelli@entando.com>
2023-04-19 11:27:35 +0200

    ENG-4776 porting operator tomcat selection fix ppl

--- .ent/enp-build.sh
+++ .ent/enp-build.sh
@@ -7,4 +7,5 @@ _log_i "Running build"
 
 ./generate-manifests.sh \
   --version "${ENTANDO_PRJ_VERSION}" \
-  --mainline "$ENTANDO_OPT_MAINLINE_VERSION"
+  --mainline "$ENTANDO_OPT_MAINLINE_VERSION" \
+  --controller-coordinator-branch release/7.1
