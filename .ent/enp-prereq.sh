e5f749eebffb2f4a8cdd19c55091484d28101987
Breno de Souza <brenoqueirozvds@gmail.com>
2023-06-22 12:23:06 -0300

    IT-374 Hardcoding opm url and pkg

--- .ent/enp-prereq.sh
+++ .ent/enp-prereq.sh
@@ -11,8 +11,10 @@ export OS=$(uname | awk '{print tolower($0)}')
 #curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # OLM
-curl -sLO "$ENTANDO_OPT_OPM_CLI_URL"
-tar xvfz "$ENTANDO_OPT_OPM_CLI_PKG"
+#curl -sLO "$ENTANDO_OPT_OPM_CLI_URL"
+#tar xvfz "$ENTANDO_OPT_OPM_CLI_PKG"
+curl -sLO "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest-4.9/opm-linux-4.9.59.tar.gz"
+tar xvfz opm-linux-4.9.59.tar.gz
 chmod +x opm && mv opm /usr/local/bin/
 
 opm version
